7daad716e11c82719b32a48ba441ab6a412f7d31
Akindele Michael <akindelemichael65@gmail.com>
2024-01-05 22:17:52 +0100

    build weather functions

--- example3/lib/weather.dart
+++ example3/lib/weather.dart
@@ -9,6 +9,8 @@ enum City {
   tokyo,
 }
 
+const unknownWeatherEmoji = '🤷🏾‍♂️';
+
 Future<WeatherEmoji> getWeather(City city) {
   return Future.delayed(
     const Duration(seconds: 1),
@@ -20,13 +22,58 @@ Future<WeatherEmoji> getWeather(City city) {
   );
 }
 
+final currentCityProvider = StateProvider<City?>((ref) => null);
+
+final weatherProvider = FutureProvider<WeatherEmoji>((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  }
+  return unknownWeatherEmoji;
+});
+
 class WeatherPage extends ConsumerWidget {
   const WeatherPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final weather = ref.watch(weatherProvider);
     return Scaffold(
-      appBar: AppBar(title: Text('HomePage')),
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Column(
+        children: [
+          weather.when(
+              data: (data) => Text(
+                    data,
+                    style: const TextStyle(fontSize: 50),
+                  ),
+              error: (_, __) => const Text(
+                    unknownWeatherEmoji,
+                    style: TextStyle(fontSize: 50),
+                  ),
+              loading: () => const CircularProgressIndicator()),
+          Expanded(
+            child: ListView.builder(
+                itemCount: City.values.length,
+                itemBuilder: (ctx, index) {
+                  final city = City.values[index];
+                  final isSelected = city == ref.watch(currentCityProvider);
+
+                  return ListTile(
+                    title: Text(
+                      city.name,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    trailing: isSelected ? const Icon(Icons.check) : null,
+                    onTap: () =>
+                        ref.read(currentCityProvider.notifier).state = city,
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
