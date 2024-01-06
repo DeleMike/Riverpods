cb017ae5e530d3b7ec656a1215321206bf608e96
Akindele Michael <akindelemichael65@gmail.com>
2024-01-06 15:57:22 +0100

    create a simple favorite movie app

--- example6/lib/main.dart
+++ example6/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,114 +13,197 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+@immutable
+class Film {
+  final String id;
   final String title;
+  final String description;
+  final bool isFavorite;
+
+  const Film({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isFavorite,
+  });
+
+  Film copy({required isFavorite}) => Film(
+        id: id,
+        title: title,
+        description: description,
+        isFavorite: isFavorite,
+      );
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  String toString() {
+    return 'Film(id: $id, title:$title, description:$description, isFavorite:$isFavorite)';
+  }
+
+  @override
+  bool operator ==(covariant Film other) =>
+      id == other.id && isFavorite == other.isFavorite;
+
+  @override
+  int get hashCode => Object.hashAll([id, isFavorite]);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const allFilms = [
+  Film(
+    id: '1',
+    title: 'The Shawshank Redemption',
+    description: 'Description of the Shawshank Redemption',
+    isFavorite: false,
+  ),
+  Film(
+    id: '2',
+    title: 'The GodFather',
+    description: 'Description for the GodFather',
+    isFavorite: false,
+  ),
+  Film(
+    id: '3',
+    title: 'The GodFather: Part II',
+    description: 'Description for the GodFather II',
+    isFavorite: false,
+  ),
+  Film(
+    id: '4',
+    title: 'The Dark Knight',
+    description: 'Description of the Dark Knight',
+    isFavorite: false,
+  ),
+];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class FilmsNotifier extends StateNotifier<List<Film>> {
+  FilmsNotifier() : super(allFilms);
+
+  void updateFilm(Film film, bool isFavorite) {
+    state = state
+        .map((mFilm) =>
+            mFilm.id == film.id ? mFilm.copy(isFavorite: isFavorite) : mFilm)
+        .toList();
   }
+}
+
+enum FavoriteStatus {
+  all,
+  favorite,
+  notFavorite,
+}
+
+final favoriteStatusProvider = StateProvider<FavoriteStatus>((ref) {
+  return FavoriteStatus.all;
+});
+
+final allFilmsProvider =
+    StateNotifierProvider<FilmsNotifier, List<Film>>((ref) => FilmsNotifier());
+
+final favoriteFilmProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where((mFilm) => mFilm.isFavorite),
+);
+
+final notFavoriteFilmProvider = Provider<Iterable<Film>>(
+  (ref) => ref.watch(allFilmsProvider).where((mFilm) => !mFilm.isFavorite),
+);
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Column(
+        children: [
+          const Center(child: FilterWidget()),
+          Consumer(builder: (contex, ref, _) {
+            final filter = ref.watch(favoriteStatusProvider);
+
+            switch (filter) {
+              case FavoriteStatus.all:
+                return FilmsList(
+                  provider: allFilmsProvider,
+                );
+                case FavoriteStatus.favorite:
+                return FilmsList(
+                  provider: favoriteFilmProvider,
+                );
+                case FavoriteStatus.notFavorite:
+                return FilmsList(
+                  provider: notFavoriteFilmProvider,
+                );
+            
+            }
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class FilterWidget extends StatelessWidget {
+  const FilterWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Consumer(
+      builder: (context, ref, child) {
+        return DropdownButton(
+          value: ref.watch(favoriteStatusProvider),
+          items: FavoriteStatus.values
+              .map(
+                (status) => DropdownMenuItem<FavoriteStatus>(
+                  value: status,
+                  child: Text(status.name),
+                ),
+              )
+              .toList(),
+          onChanged: (value) {
+            ref.watch(favoriteStatusProvider.notifier).state = value!;
+          },
+        );
+      },
     );
+  }
+}
+
+class FilmsList extends ConsumerWidget {
+  final AlwaysAliveProviderBase<Iterable<Film>> provider;
+  const FilmsList({required this.provider, super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final films = ref.watch(provider);
+    return Expanded(
+        child: ListView.builder(
+      itemBuilder: (ctx, index) {
+        final film = films.elementAt(index);
+        final favoriteIcon = film.isFavorite
+            ? const Icon(Icons.favorite)
+            : const Icon(
+                Icons.favorite_border,
+              );
+        return ListTile(
+            title: Text(film.title),
+            subtitle: Text(film.description),
+            trailing: IconButton(
+              icon: favoriteIcon,
+              onPressed: () {
+                final isFavorite = !film.isFavorite;
+                ref
+                    .read(allFilmsProvider.notifier)
+                    .updateFilm(film, isFavorite);
+              },
+            ));
+      },
+      itemCount: films.length,
+    ));
   }
 }
